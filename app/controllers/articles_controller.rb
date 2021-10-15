cd61a923695c4be51b1c029bededb593ea438c06
sun pochin <sunpochin@gmail.com>
2021-10-15 18:13:22 +0800

    saving newly added articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,18 @@ class ArticlesController < ApplicationController
     # byebug
     # instance variable, @
     @article = Article.find(params[:id])
-    print "Article: " + @article.title + "\n"
+    # print "Article: " + @article.title + "\n"
   end
 
+  def new
+  end
+
+  def create
+    # render plain: params[:article].inspect
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+
+    # redirect_to article_path(@article)
+    redirect_to @article
+  end
 end
