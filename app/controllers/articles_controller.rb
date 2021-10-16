18aba380a61c548fa605a44cfcb64d0514eb226c
sun pochin <sunpochin@gmail.com>
2021-10-16 14:27:42 +0800

    adding update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,14 +12,37 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     # render plain: params[:article].inspect
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-
-    # redirect_to article_path(@article)
-    redirect_to @article
+    if @article.save
+      flash[:notice] = "Article created successfully"
+      # redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+
+  def edit
+    # byebug
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    # byebug
+    @article = Article.find(params[:id])
+    
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+
 end
