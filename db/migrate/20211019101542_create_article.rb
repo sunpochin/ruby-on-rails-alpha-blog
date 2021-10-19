f5b0c5766d0ab0a5122a86840e349952e5787b20
sun pochin <sunpochin@gmail.com>
2021-10-19 19:12:23 +0800

    adding create_articles

--- db/migrate/20211019101542_create_article.rb
+++
@@ -1,9 +0,0 @@
-class CreateArticle < ActiveRecord::Migration[6.1]
-  def change
-    create_table :articles do |t|
-      add_column :articles, :created_at, :datetime
-      add_column :articles, :updated_at, :datetime
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_120246) do
+ActiveRecord::Schema.define(version: 2021_10_19_102456) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
