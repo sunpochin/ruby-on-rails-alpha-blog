c10fff5ef505bbe5b9d019190363201e2abbf50c
sun pochin <sunpochin@gmail.com>
2021-10-19 19:23:09 +0800

    adding migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_102456) do
+ActiveRecord::Schema.define(version: 2021_10_19_111719) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
