5774a61550c9555883a04cb18a8c3026195764bb
sun pochin <sunpochin@gmail.com>
2021-10-23 20:43:27 +0800

    adding edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,25 @@ class UsersController < ApplicationController
     return "hello"
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account has been successfully updated."
+      redirect_to articles_path
+      # redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def create
     # byebug
     @user = User.new(user_params)
