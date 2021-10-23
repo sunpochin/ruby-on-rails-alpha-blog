d1d5493e201a2b699ce732cae1f6e20b25f784c9
sun pochin <sunpochin@gmail.com>
2021-10-23 22:58:42 +0800

    adding gravatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user)
+    hash = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_url = "http://gravatar.com/avatar/#{hash}"
+    image_tag(gravatar_url, alt: user.username)
+  end
+
 end
