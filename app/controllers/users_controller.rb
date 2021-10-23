9984c244e3c10cc1e5c9b96b165bceecdf4d0d53
sun pochin <sunpochin@gmail.com>
2021-10-23 19:10:54 +0800

    coding up sign up at index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,26 @@ class UsersController < ApplicationController
   end
 
   def create
+    # byebug
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "Signed up!"
+      flash[:notice] = "#{@user.username} you have successfully signed up. Welcome to the blog."
+      redirect_to articles_path
     else
       render :new
     end
+    # @user = User.new(user_params)
+    # if @user.save
+    #   redirect_to root_path, notice: "Signed up!"
+    # else
+    #   render :new
+    # end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
-  # get 'login', to: 'sessions#new'
-  post 'users', to: 'users#create'
+  # post 'users', to: 'users#create'
+  # resources :users, except: [:new]
+  resources :users
+
 end
 
 
