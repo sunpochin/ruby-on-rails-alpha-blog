8735520408b6c60a8f9867afb3eb4378ceca0fe1
sun pochin <sunpochin@gmail.com>
2021-10-24 12:34:37 +0800

    show user and profile image and user articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 80 } )
     hash = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "http://gravatar.com/avatar/#{hash}"
-    image_tag(gravatar_url, alt: user.username)
+    size = options[:size]
+    gravatar_url = "http://gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded mx-auto d-block shadow")
   end
 
 end
