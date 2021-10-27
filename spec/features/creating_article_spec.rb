b6fa2de55288b999e5fa78e865c5e5a4e0316f55
sun pochin <sunpochin@gmail.com>
2021-10-27 21:06:21 +0800

    fix circleci

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -4,6 +4,11 @@ RSpec.feature "Creating Articles" do
   scenario "A user creates a new article" do
     visit "/"
 
-    click_link ""
+    click_link "New Article"
+
+    fill_in "Title", with: "Creating a blog"
+    fill_in "Body", with: "Lorem Ipsum 某些內容"
+
+    click_button "Create Article"
   end
 end
